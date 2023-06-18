1ee5ec96f8c3cb04c792ca0cd00a697c01684201
Prisca <priscaaudyaa@gmail.com>
2023-06-18 18:56:59 +0700

    first commit

---
+++ satu.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Masukkan bilangan genap positif: ', (input) => {
+  const x = parseInt(input);
+
+  if (x < 0) {
+    console.log('Tidak bisa input bilangan negatif');
+  } else if (x % 2 !== 0) {
+    console.log('Tidak bisa input bilangan ganjil');
+  } else {
+    const result = Math.sqrt(x);
+    console.log(`Akar pangkat 2 dari ${x} adalah ${result}`);
+  }
+
+  rl.close();
+});
+
+
+
+
+
